b81b16af47399833c0412eaa1aa8c48f2d31fe58
Viktor Johansen <viktorjohansen@outlook.com>
2026-02-22 10:33:44 -0500

    Added a Game over screen with two buttons and a simple stats breakdown, improved transition code a bit, Swapped exit button text from "Exit" to "Quit game" to reduce confusion

---
+++ Scripts/GameOver.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+using System.Numerics;
+
+public partial class GameOver : Control
+{
+    [Export]
+    Label StatsLabel;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        StatsLabel.Text = "Great shootin', soldier!" + "\n"
+                        + "Total waves survived: " + "[Waves]" + "\n"
+                        + "Total enemies killed: " + "[Kill Count]";
+    }
+
+    public async void MainMenu()
+    {
+        Transition.Instance.TransitionBetweenScenes();
+        await ToSignal(GetTree().CreateTimer(1), "timeout");
+         GetTree().ChangeSceneToFile("res://Scenes/TitleScreen.tscn");
+    }
+
+    public void Exit()
+    {
+        GetTree().Quit();
+    }
+
+}
--- Scripts/Transition.cs
+++ Scripts/Transition.cs
@@ -15,13 +15,14 @@ public partial class Transition : CanvasLayer
     {
         base._Ready();
         Instance ??= this;
+        Rect.Visible = false;
     }
 
     public async void Epilepsy()
     {
+        Rect.Visible = true;
         Fade.Play("Fade_To_Normal");
         await ToSignal(GetTree().CreateTimer(1), "timeout");
-
         Rect.Visible = false;
     }
 
